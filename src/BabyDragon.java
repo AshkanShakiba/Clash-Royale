25c0ff042453cea4e6fa11c1f067333cdc6bd13c
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 20:41:18 +0430

    Adding BabyDragon

---
+++ src/BabyDragon.java
@@ -0,0 +1,32 @@
+public class BabyDragon extends Warrior{
+    public BabyDragon(User user){
+        switch (user.getLevel()){
+            case 1:
+                hp=800;
+                damage=100;
+                break;
+            case 2:
+                hp=880;
+                damage=110;
+                break;
+            case 3:
+                hp=968;
+                damage=121;
+                break;
+            case 4:
+                hp=1064;
+                damage=133;
+                break;
+            case 5:
+                hp=1168;
+                damage=146;
+        }
+        hitSpeed=1.8;
+        speed=4;
+        target=2;
+        range=3;
+        areaSplash=true;
+        count=1;
+        cost=4;
+    }
+}
