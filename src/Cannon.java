56c6c4294f7d6cc1f6aef56487bdafe9b97913ff
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 21:21:17 +0430

    Adding Cannon

---
+++ src/Cannon.java
@@ -0,0 +1,30 @@
+public class Cannon extends Building{
+    public Cannon(User user){
+        switch (user.getLevel()){
+            case 1:
+                hp=380;
+                damage=60;
+                break;
+            case 2:
+                hp=418;
+                damage=66;
+                break;
+            case 3:
+                hp=459;
+                damage=72;
+                break;
+            case 4:
+                hp=505;
+                damage=79;
+                break;
+            case 5:
+                hp=554;
+                damage=87;
+        }
+        hitSpeed=0.8;
+        target=0;
+        range=5.5;
+        lifetime=30;
+        cost=6;
+    }
+}
