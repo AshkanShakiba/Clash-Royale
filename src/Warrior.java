47b8b3464ebe44c7e90e0cb6cbb4c8503cffda45
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 20:25:37 +0430

    Adding Barbarians

--- src/Warrior.java
+++ src/Warrior.java
@@ -2,7 +2,7 @@ public class Warrior extends Card {
     protected int hp;
     protected int damage;
     protected double hitSpeed;
-    protected int speed;
+    protected int speed; // 2: Slow, 3: Medium, 4: Fast
     protected int target; // 0: Ground, 1: Air, 2: Both
     protected int range; // 0: Melee
     protected boolean areaSplash;
