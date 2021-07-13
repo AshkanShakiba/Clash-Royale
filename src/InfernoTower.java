f412c67f05b83245c4624bb6863a8830d6d5e8eb
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 21:28:25 +0430

    Adding InfernoTower

---
+++ src/InfernoTower.java
@@ -0,0 +1,30 @@
+public class InfernoTower extends Building{
+    public InfernoTower(User user){
+        switch (user.getLevel()){
+            case 1:
+                hp=800;
+                damage=20;
+                break;
+            case 2:
+                hp=880;
+                damage=22;
+                break;
+            case 3:
+                hp=968;
+                damage=24;
+                break;
+            case 4:
+                hp=1064;
+                damage=26;
+                break;
+            case 5:
+                hp=1168;
+                damage=29;
+        }
+        hitSpeed=0.4;
+        target=2;
+        range=6;
+        lifetime=40;
+        cost=5;
+    }
+}
