77a49cbfba98c5522e88b7471c88536d4882fdb5
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 21:44:10 +0430

    Adding Arrows

---
+++ src/Arrows.java
@@ -0,0 +1,22 @@
+public class Arrows extends Charm{
+    public Arrows(User user){
+        switch (user.getLevel()){
+            case 1:
+                areaDamage=144;
+                break;
+            case 2:
+                areaDamage=156;
+                break;
+            case 3:
+                areaDamage=174;
+                break;
+            case 4:
+                areaDamage=189;
+                break;
+            case 5:
+                areaDamage=210;
+        }
+        radius=4;
+        cost=3;
+    }
+}
