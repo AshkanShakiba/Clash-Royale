ba19a0ef1d5d57a3f8109406c72c6ebcd5ce57a5
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 21:20:55 +0430

    Adding Buliding fields

--- src/Building.java
+++ src/Building.java
@@ -1,2 +1,8 @@
 public class Building extends Card {
+    protected int hp;
+    protected int damage;
+    protected double hitSpeed;
+    protected int target; // 0: Ground, 1: Air, 2: Both, 3: Buildings
+    protected double range; // 0: Melee
+    protected int lifetime;
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -4,7 +4,7 @@ public class Warrior extends Card {
     protected double hitSpeed;
     protected int speed; // 2: Slow, 3: Medium, 4: Fast
     protected int target; // 0: Ground, 1: Air, 2: Both, 3: Buildings
-    protected int range; // 0: Melee
+    protected double range; // 0: Melee
     protected boolean areaSplash;
     protected int count;
 }
