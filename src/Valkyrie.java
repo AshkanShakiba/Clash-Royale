81f8b487c1b8e3d1117f9b1fd4854bab29f4d17a
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-13 20:59:43 +0430

    Adding Valkyrie

---
+++ src/Valkyrie.java
@@ -0,0 +1,32 @@
+public class Valkyrie extends Warrior{
+    public Valkyrie(User user){
+        switch (user.getLevel()){
+            case 1:
+                hp=880;
+                damage=120;
+                break;
+            case 2:
+                hp=968;
+                damage=132;
+                break;
+            case 3:
+                hp=1064;
+                damage=145;
+                break;
+            case 4:
+                hp=1170;
+                damage=159;
+                break;
+            case 5:
+                hp=1284;
+                damage=175;
+        }
+        hitSpeed=1.5;
+        speed=3;
+        target=0;
+        range=0;
+        areaSplash=true;
+        count=1;
+        cost=4;
+    }
+}
--- src/Warrior.java
+++ src/Warrior.java
@@ -3,7 +3,7 @@ public class Warrior extends Card {
     protected int damage;
     protected double hitSpeed;
     protected int speed; // 2: Slow, 3: Medium, 4: Fast
-    protected int target; // 0: Ground, 1: Air, 2: Both
+    protected int target; // 0: Ground, 1: Air, 2: Both, 3: Buildings
     protected int range; // 0: Melee
     protected boolean areaSplash;
     protected int count;
