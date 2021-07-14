21fb5b34a9c1d4aa09659d023d0c6d6581897b80
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-14 23:42:42 +0430

    Adding select method to Game controller

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,16 @@
-public class Game {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
 
+public class Game {
+    @FXML
+    GridPane gridPane;
+
+    public void select(ActionEvent event) {
+        Node clickedNode = (Node) event.getSource();
+        Integer colIndex = GridPane.getColumnIndex(clickedNode);
+        Integer rowIndex = GridPane.getRowIndex(clickedNode);
+        System.out.println("Mouse clicked cell: " + colIndex + " And: " + rowIndex);
+    }
 }
