e8f25e57583c05c0e90a3adc2f0dc734e80440c9
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-15 16:59:43 +0430

    Addint next card

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,8 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
+import java.util.ArrayList;
+
 public class Game {
     @FXML
     private AnchorPane leftPane;
@@ -21,17 +23,34 @@ public class Game {
     private Button card3;
     @FXML
     private Button card4;
+    @FXML
+    private Button next;
 
+    private User user;
     private double x, y;
-    private Card selected;
+    private Card nextCard;
+    private Card selectedCard;
+    private ArrayList<Card> availableCards;
+
+    public void construct(User user) {
+        this.user = user;
+    }
 
     public void select(ActionEvent event) {
-        Button selected=(Button) event.getSource();
-        selected.setStyle(selected.getStyle()+"-fx-background-image: url('assets/giant.png');");
+        Button selected = (Button) event.getSource();
+        selected.setStyle(selected.getStyle() + "-fx-background-image: url('assets/giant.png');");
     }
 
     public void click(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    private void setNextCard() {
+        Card card = user.getRandomCard();
+        while (availableCards.contains(card)) {
+            card = user.getRandomCard();
+        }
+        nextCard = card;
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,15 +1,23 @@
+import java.security.SecureRandom;
+
 public class User {
     private int xp;
     private int level;
     private Card[] cards;
+    private SecureRandom random;
 
     public User() {
         xp = 0;
         level = 1;
         cards = new Card[8];
+        random = new SecureRandom();
     }
 
     public int getLevel() {
         return level;
+    }
+
+    public Card getRandomCard() {
+        return cards[random.nextInt(8)];
     }
 }
