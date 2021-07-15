dcfeeafc8755eaa7005714ac4b2b990019bd37f5
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-15 16:27:03 +0430

    Completing game pane

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class Game {
     @FXML
@@ -8,6 +10,16 @@ public class Game {
     private AnchorPane middlePane;
     @FXML
     private AnchorPane rightPane;
+    @FXML
+    private GridPane cardsPane;
+    @FXML
+    private Button card1;
+    @FXML
+    private Button card2;
+    @FXML
+    private Button card3;
+    @FXML
+    private Button card4;
 
     private double x, y;
     private Card selected;
