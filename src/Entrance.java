7f3bb3b054ca5faa7ce3c33f67c486cd0ebaf0ae
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-17 02:08:44 +0430

    Adding temporary entrance to game by about method

--- src/Entrance.java
+++ src/Entrance.java
@@ -1,10 +1,12 @@
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -28,7 +30,25 @@ public class Entrance {
     }
 
     public void about(ActionEvent event) {
-
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+            Parent root = loader.load();
+            Game game = loader.getController();
+            User user=new User();
+            game.construct(user);
+            root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    game.click(event.getSceneX(), event.getSceneY());
+                }
+            });
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void exit(ActionEvent event) {
