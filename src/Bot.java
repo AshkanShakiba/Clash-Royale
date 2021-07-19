cf400e98f7095d03e9932234110683a14a1e63d1
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-20 00:10:08 +0430

    Starting to implement smart bot

--- src/Bot.java
+++ src/Bot.java
@@ -3,14 +3,16 @@ import java.util.ArrayList;
 
 public class Bot {
     private Game game;
+    private int level;
     private double elixir;
     private Card nextCard;
     private ArrayList<Card> cards;
     private SecureRandom random;
 
-    public Bot(Game game){
+    public Bot(Game game,int level){
         elixir=4;
         this.game=game;
+        this.level=level;
         cards=new ArrayList<>();
         random=new SecureRandom();
         for(int i=0;i<8;i++){
@@ -27,7 +29,45 @@ public class Bot {
     }
 
     public void move(){
+        switch (level){
+            case 1:
+                move1();
+                break;
+            case 2:
+                move2();
+                break;
+        }
+    }
+
+    private void move1(){
         Card card=cards.get(random.nextInt(8));
+        putCard(card);
+    }
+
+    private void move2(){
+        Card card=cards.get(random.nextInt(8));
+        ArrayList<Warrior> warriors=game.getPlayerWarriors();
+        for(Warrior warrior:warriors){
+            if(warrior instanceof Giant){
+                card=Card.BARBARIANS;
+            }
+            if(warrior instanceof BabyDragon){
+                card=Card.BABYDRAGON;
+            }
+            if(warrior instanceof Barbarians){
+                card=Card.VALKYRIE;
+            }
+            if(warrior instanceof MiniPekka){
+                card=Card.ARCHERS;
+            }
+            if(warrior instanceof Cannon){
+                card=Card.FIREBALL;
+            }
+            putCard(card,warrior.getArrayX(),warrior.getArrayY());
+        }
+    }
+
+    private void putCard(Card card){
         if(elixir<card.getCost()) return;
         int X = random.nextInt(18);
         int Y = random.nextInt(14);
@@ -35,6 +75,20 @@ public class Bot {
         Warrior warrior = card.getWarrior(user, X, Y-14);
         game.getTeamsMap().put(warrior, 1);
         game.getMap()[X][Y] = warrior;
+        game.getWarriorsInTheMap().add(warrior);
+        System.out.println("***"+card.name()+"***");
+        warrior.buildImageView("red");
+        game.getMiddlePane().getChildren().add(warrior.imageView);
+        elixir -= card.getCost();
+        Game.playAudio(card.getAudio());
+    }
+
+    private void putCard(Card card,int x,int y){
+        if(elixir<card.getCost()) return;
+        User user=new User();
+        Warrior warrior = card.getWarrior(user, x, y);
+        game.getTeamsMap().put(warrior, 1);
+        game.getMap()[x][y] = warrior;
         game.getWarriorsInTheMap().add(warrior);
         System.out.println("***"+card.name()+"***");
         warrior.buildImageView("red");
