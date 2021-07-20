1f64417a14da9e386c00549246db5289004f6c74
AmirerfanTim <Amirerfantim@gmail.com>
2021-07-21 03:25:50 +0430

    Changing design pattern

--- src/Troop.java
+++ src/Troop.java
@@ -1,16 +1,11 @@
 public abstract class Troop extends RealWarriors {
     protected int speed; // 2: Slow, 3: Medium, 4: Fast
-    protected int target; // 0: Ground, 1: Air, 2: Both, 3: Buildings
     protected boolean areaSplash;
     protected int count;
 
 
     public int getSpeed() {
         return speed;
-    }
-
-    public int getTarget() {
-        return target;
     }
 
     public boolean isAreaSplash() {
