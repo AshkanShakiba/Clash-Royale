833097bb48e98259688dfc6b959ec52963caccdc
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-20 19:44:32 +0430

    Adding cheat code checker

--- src/Menu.java
+++ src/Menu.java
@@ -2,10 +2,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -13,6 +15,9 @@ import javafx.util.Duration;
 import java.io.IOException;
 
 public class Menu {
+    @FXML
+    private PasswordField cheatField;
+
     public void battle(ActionEvent event) {
         try {
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -63,6 +68,23 @@ public class Menu {
             stage.show();
         } catch (IOException exception) {
             exception.printStackTrace();
+        }
+    }
+
+    public void check(ActionEvent event){
+        String cheatCode=cheatField.getText();
+        if(cheatCode.equalsIgnoreCase("theme")){
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("themes.fxml"));
+                Parent root = loader.load();
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                //stage.initStyle(StageStyle.DECORATED);
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         }
     }
 }
