8da8108e8fdfcfad80c77d2c75d3fc59c1cad16c
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-22 19:47:50 +0430

    Completing LeaderBoard controller

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -1,8 +1,15 @@
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -126,7 +133,16 @@ public class LeaderBoard implements Initializable {
 
     }
 
-
-
-
+    public void back(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("menu.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
