d1e9e5338d9a62e876c7d3ad50856756455758c1
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-23 01:10:22 +0430

    Implementing VikiBarbarians

--- src/Barbarians.java
+++ src/Barbarians.java
@@ -1,45 +1,75 @@
 import javafx.scene.image.ImageView;
 
 public class Barbarians extends Troop {
+    private static int[] hpArray = new int[]{300, 330, 363, 438, 480};
+    private static int[] damageArray = new int[]{75, 82, 90, 99, 109};
+
+    private static double hitSpeedStatic = 1.5;
+    private static int speedStatic = 3;
+
+    private static String imageBluePath = "sprites/BarbariansBlue.gif";
+    private static String imageRedPath = "sprites/BarbariansRed.gif";
+
+    private static boolean isViki = false;
+    private static boolean isQuick = false;
+
     public Barbarians(User user, int x, int y) {
         this.arrayX = x;
         this.arrayY = y;
         switch (user.getLevel()) {
             case 1:
-                hp = 300;
-                damage = 75;
+                hp = hpArray[0];
+                damage = damageArray[0];
                 break;
             case 2:
-                hp = 330;
-                damage = 82;
+                hp = hpArray[1];
+                damage = damageArray[1];
                 break;
             case 3:
-                hp = 363;
-                damage = 90;
+                hp = hpArray[2];
+                damage = damageArray[2];
                 break;
             case 4:
-                hp = 438;
-                damage = 99;
+                hp = hpArray[3];
+                damage = damageArray[3];
                 break;
             case 5:
-                hp = 480;
-                damage = 109;
+                hp = hpArray[4];
+                damage = damageArray[4];
         }
-        hitSpeed = 1.5;
-        speed = 3;
+        hitSpeed = hitSpeedStatic;
+        speed = speedStatic;
         target = 0;
         range = 0;
         areaSplash = false;
         count = 4;
         zone = 0;
-        imageBlue = "sprites/BarbariansBlue.gif";
-        imageRed = "sprites/BarbariansRed.gif";
-        imageWidth=80;
-        imageHeight=80;
+        imageBlue = imageBluePath;
+        imageRed = imageRedPath;
+        imageWidth = 80;
+        imageHeight = 80;
     }
 
     @Override
     public String toString() {
         return "Barbarians";
+    }
+
+    public static void upgradeToViki() {
+        hitSpeedStatic = 1.5;
+        speedStatic = 3;
+
+        hpArray = new int[]{450, 495, 545, 657, 720};
+        damageArray = new int[]{113, 123, 135, 149, 164};
+
+        imageBluePath = "sprites/VikiBarbariansBlue.gif";
+        imageRedPath = "sprites/VikiBarbariansRed.gif";
+
+        isViki = true;
+        isQuick = false;
+    }
+
+    public static boolean isViki() {
+        return isViki;
     }
 }
